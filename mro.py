6665596fc6aa5bdb452fe66cb4c65f9ec4534b47
hussain ali <hussainali223337@gmail.com>
2017-11-03 17:34:42 +0200

    cleaned up commants

--- mro.py
+++ mro.py
@@ -1,5 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
-##############################################################################
+
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2013-2014 CodUP (<http://codup.com>).
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+
 
 import time
 
--- stock.py
+++ stock.py
@@ -1,5 +1,5 @@
 ﻿# -*- coding: utf-8 -*-
-##############################################################################
+
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2014 CodUP (<http://codup.com>).
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+
 
 from openerp.osv import osv
 from openerp import workflow
